7a2a2f912f14fb4072fafd11e2a2cd3d59f51346
diamondhead2017 <awake.vote@gmail.com>
2018-01-26 21:43:19 +0900

    1/26

--- script.js
+++ script.js
@@ -1,22 +1,78 @@
-function menseki(num){
-      return"面積は"+(num*num*3.14)+"c㎡です。";
+for(var i=1;i<=5;i++){
     
-  } 
-    document.write(menseki(5));
-    document.write("<br>");
-        document.write(menseki(7));
-         document.write("<br>");
-        document.write(menseki(10));
-         document.write("<br>");
-        
-
-function grupu(a,b){
-    return"合計金額は"+(a*500+b*200)+"円です。";
-    
+document.write("★");
 }
-document.write(grupu(2,4));
+document.write("<br><br><br>");
+
+for(var i=1;i<=2;i++){
+    for(var b=1;b<=3;b++){
+document.write("★");
+        }
     document.write("<br>");
-        document.write(grupu(1,5));
-         document.write("<br>");
-        document.write(grupu(3,7));
-         document.write("<br>");
+}
+document.write("<br><br><br>");
+
+for(var a=1;a<=2;a++){
+    for(var b=1;b<=5;b++){
+        document.write("☆")
+    }
+    document.write("<br>")
+}
+
+document.write("<br><br><br>");
+
+for(var a=1;a<=4;a++){
+    for(var b=1;b<=5;b++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+
+document.write("<br><br><br>");
+
+for(var a=1;a<=4;a++){
+    for(var b=1;b<=3;b++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+document.write("<br><br><br>");
+
+for(var a=1;a<=3;a++){
+    for(var b=1;b<=3;b++){
+        if(b%2==0){
+            document.write("☆");
+        }else{
+        document.write("★");
+        }
+    }
+    document.write("<br>");
+}
+document.write("<br><br><br>");
+
+for(var a=1;a<=5;a++){
+    for(var b=1;b<=5;b++){
+        if(b%2==0){
+            document.write("☆");
+            //２で割り切れる場合
+        }else{
+        document.write("★");
+            //そうでない場合
+        }
+    }
+    document.write("<br>");
+}
+document.write("<br><br><br>");
+
+for(var a=1;a<=5;a++){
+    for(var b=1;b<=a;b++){
+        //bにaを代入するとなった。
+        document.write("★");
+    }
+    document.write("<br>");
+}
+
+
+
+
+
